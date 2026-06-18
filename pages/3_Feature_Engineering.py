f1685df924e31b358c26ea036b211c0d6fcfd2ef
TwasimodoK <145058232+TwasimodoK@users.noreply.github.com>
2026-06-18 14:45:54 +0530

    Add feature engineering options to Streamlit app

--- pages/3_Feature_Engineering.py
+++ pages/3_Feature_Engineering.py
@@ -1,3 +1,49 @@
 import streamlit as st
 
 st.title("Feature Engineering")
+
+left,center,right = st.columns([1,2,1])
+
+with left:
+
+    st.subheader("Selected Data")
+
+    st.checkbox("Geochemistry",True)
+    st.checkbox("Gravity")
+    st.checkbox("Magnetic")
+
+with center:
+
+    st.subheader("Display")
+
+    st.info("Plots appear here")
+
+with right:
+
+    st.subheader("Feature Engineering")
+
+    st.selectbox(
+        "Geochemistry",
+        [
+            "None",
+            "CLR",
+            "ILR",
+            "RPCA",
+            "Robust PCA"
+        ]
+    )
+
+    st.selectbox(
+        "Geophysics",
+        [
+            "None",
+            "VD",
+            "ASVD",
+            "Log Transform"
+        ]
+    )
+
+    st.checkbox("Normalize")
+    st.checkbox("Correlation Filter")
+
+st.button("Next")
